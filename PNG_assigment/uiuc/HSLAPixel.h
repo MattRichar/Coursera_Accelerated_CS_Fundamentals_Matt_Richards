02c2c85dc415f0db44526659e8ca75728c4ecdaa
MattRichar <matt.richards47@yahoo.com>
2021-03-01 00:19:23 +0000

    PNG_assignment_solution

---
+++ PNG_assigment/uiuc/HSLAPixel.h
@@ -0,0 +1,28 @@
+/**
+ * @file HSLAPixel.h
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+namespace uiuc {
+  class HSLAPixel{
+    
+    public:
+      double h;//Hue, Ranging in value between 0 and 360
+      double s;//Saturation, Ranging in value between 0 and 1
+      double l;//Luminance, Ranging in value between 0 and 1
+      double a;//Alpha, Ranging in value between 0 and 1
+      
+  };
+
+  // Put your HSLAPixel class definition here.
+  // (Remember to end it with a semicolon!)
+
+
+}
